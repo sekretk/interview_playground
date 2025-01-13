c5f6dc3e1559cdb4351914db9ae714267f71f3f2
sekretk <boyko.constantine@gmail.com>
2025-01-13 05:49:40 +0300

    cases

---
+++ test.tsx
@@ -0,0 +1,32 @@
+branch main
+
+branch feature1
+- commit 1 (file1+-. file2+-)
+- commit 2 (file1+-. file2+-)
+- commit 3 (file1+-. file2+-)
+- commit 4 (file1+-. file2+-)
+- commit 5 (file1+-. file2+-)
+- commit 6 (file1+-. file2+-)
+- commit 7 (file1+-. file2+-)
+
+- commulativeCommit (file1+-. file2+-)
+
+
+
+----------------
+create feature1 (file1+-)
+create feature2 (file2+-)
+
+
+//SOLUTION1
+undo last (not worked with >2)
+//solution2
+cherry-pick all commits? where?
+//
+
+//git reset --soft
+//git rebase -i
+
+
+//git stash?
+//git worktree?
