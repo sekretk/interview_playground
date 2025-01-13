feb1a5a615b075826f482a4724d00b558fac434a
sekretk <boyko.constantine@gmail.com>
2025-01-13 09:15:13 +0300

    some mew

--- includes.ts
+++ includes.ts
@@ -1,13 +1,5 @@
 const arr = [1,2,3];
 // const includes = arr.includes.bind(arr);//type?
 const includes =  (item: number) => arr.includes(item);
-//Array buildin functions?
-//sum arr with reduce?
 
 includes(1);//failed
-
-
-const sum = arr.reduce((acc, cur) => acc + cur, [])
-
-// [1,2] + [] => [1,2]
-// [] + [1, 2] => [1,2]
---
+++ reduce.ts
@@ -0,0 +1,4 @@
+//Array buildin functions?
+const arr = [1,2,3];
+
+//get sum of arr with reduce
